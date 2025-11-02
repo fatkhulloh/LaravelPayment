dfc502438c02ee85e201469ecc7812298501487a
Fatkhulloh Hol <fatkhulloh6939@gmail.com>
2025-11-02 16:17:25 +0700

    Add Register, Login and Home Ui

---
+++ resources/views/pages/beranda.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.master')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Make a payment</div>
+
+                <div class="card-body">
+                    Thih is Payment Processing System, using the most popular payment platform available.
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
